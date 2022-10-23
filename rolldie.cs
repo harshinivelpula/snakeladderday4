62a29437bcc682d4796cc2143410d20ba05e68b0
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 15:53:41 +0530

    check for die

---
+++ rolldie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeladderd4
+{
+    internal class rolldie
+    {
+        public int RollDie()
+        {
+            
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            return dice;
+        }
+        
+    }
+}
