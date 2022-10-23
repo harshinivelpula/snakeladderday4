6c83a6725e0d4dd9e32b4db12d547d51964965da
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 15:49:56 +0530

    adding program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace snakeladderd4
+{
+    class program
+
+    {
+        private static readonly object playerOne;
+
+        static void main(string[]args, singleplayer singleplayer)
+        {
+            Console.WriteLine("player One position is {playerOne}");
+            
+            
+
+        }
+    }
+}
