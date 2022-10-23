0436e99c559ddf00c00eb68a3d65c57787a58094
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 15:56:45 +0530

    check for singleplayer

---
+++ singleplayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeladderd4
+{
+    internal class singleplayer
+    {
+        public void start()
+        {
+            
+            int position = 0;
+            int playerOne;
+            
+            playerOne = position;
+            Console.WriteLine("player One position is {playerOne}");
+        }
+        public void Board()
+        {
+            
+            start();
+        }
+    }
+
+}
